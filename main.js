1571273b87b7ed110d94e76ce116452aba5be138
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-03-03 22:21:40 +0600

    json response send

--- main.js
+++ main.js
@@ -2,19 +2,14 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {   
     
-    if(req.url==="/"){
-        res.writeHead(200,{'content-type':'text/html'})   //header portion 
-        res.write("<h1> This is home page</h1>")    //body portion- show this to client in browser
-        res.end();
-    }
-    else if(req.url==="/about"){
-        res.write("<h1> This is about page</h1>")
-        res.end();
-    }
-    else {
-        res.write("404- Page not found")
-        res.end();
-    }
+        res.writeHead(200,{'content-type':'application/json'})   //header portion 
+        const user = {    //object
+        name: "Sakib",
+        profession: "Developer"
+    };
+
+    res.end(JSON.stringify(user));   // json respone convert to string
+
 });
 
 server.listen(3000, () => {   //port number 3000
