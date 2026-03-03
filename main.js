ac5f16247b9a6d3923b47c680acc66175017db52
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-03-03 21:54:50 +0600

    request respons model

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {   //server create
-    res.write("Hello from Node Server");    // send data to the browser who requested
-    res.end();    //response finished
+const server = http.createServer((req, res) => {   
+    
+    if(req.url==="/"){
+        res.writeHead(200,{'content-type':'text/html'})   //header portion 
+        res.write("<h1> This is home page</h1>")    //body portion- show this to client in browser
+        res.end();
+    }
+    else if(req.url==="/about"){
+        res.write("<h1> This is about page</h1>")
+        res.end();
+    }
+    else {
+        res.write("404- Page not found")
+        res.end();
+    }
 });
 
 server.listen(3000, () => {   //port number 3000
