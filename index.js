2c5ee57d47b3485df8cdf9a732f4e75e7b618ef3
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-03-05 21:38:58 +0600

    status code response

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const app = express();
 
+//Simple string response----------------------------------------------------
 app.get("/one", (req, res) => {
 
-    res.send("Simple string response");   // res.send mens response body
+    res.send("Simple string response");   // res.send means response body
 
 });
 
@@ -16,13 +17,26 @@ app.post("/two", (req, res) => {
 
 app.get("/three", (req, res) => {
 
-    res.end("Simple string response");   // res.end mens response ending point
+    res.end("Simple string response");   // res.end means response ending point
 
 });
 
 app.post("/four", (req, res) => {
 
     res.end("Simple string response");
+
+});
+
+//Status code response----------------------------------------------------
+app.get("/five", (req, res) => {
+
+    res.status(401).end("Unauthorize");   // res.status means status code
+
+});
+
+app.get("/six", (req, res) => {
+
+    res.status(201).end();   
 
 });
 
