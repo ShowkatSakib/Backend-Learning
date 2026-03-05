73d1944e07236fcaf3333d45ce5e0569dd86d7c2
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-03-05 20:50:42 +0600

    routing using get method

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {    //get method call  and home url call
-    res.send("Hello from Express Server");  //send method call
+    res.send("Home pahe");  //send method call
+});
+
+app.get("/about", (req, res) => {    //about url
+    res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {   //contact url
+    res.send("Contact Page");
 });
 
 app.listen(3000, () => {    //localhost port 3000
