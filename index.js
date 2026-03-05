fa427bf783a96b8616ca71c466e2bbc5770b1d3f
Md. Showkat Islam Sakib <showkatsakib02@gmail.com>
2026-03-05 20:54:07 +0600

    routing using others method

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {    //get method call  and home url call
-    res.send("Home pahe");  //send method call
+    res.send("Home pagSe");  //send method call
 });
 
-app.get("/about", (req, res) => {    //about url
+app.post("/about", (req, res) => {    //post method call and about url
     res.send("About Page");
 });
 
-app.get("/contact", (req, res) => {   //contact url
+app.put("/contact", (req, res) => {   //contact url
     res.send("Contact Page");
+});
+
+app.delete("/terms", (req, res) => {   //terms url
+    res.send("Terms Page");
 });
 
 app.listen(3000, () => {    //localhost port 3000
